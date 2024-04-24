a399da58ed022d3f7ce5de7aff04b04fe75bc85d
Steven Schulteis <schult@gmail.com>
2024-04-23 19:55:59 -0500

    Add version placeholder and app license to about dialog

--- AboutDialog.cpp
+++ AboutDialog.cpp
@@ -5,11 +5,35 @@
 
 #include "ui_AboutDialog.h"
 
+#ifndef VERSION_STRING
+#define VERSION_STRING "--"
+#endif
+
+#ifndef BUILD_NUMBER_STRING
+#define BUILD_NUMBER_STRING "--"
+#endif
+
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+
+    ui->tabWidget->setCurrentWidget(ui->tabApp);
+
+    auto version_font = ui->labelVersion->font();
+    version_font.setBold(true);
+    version_font.setPointSize(version_font.pointSize() * 2);
+    ui->labelVersion->setFont(version_font);
+    ui->labelVersion->setText(QString("Version %1").arg(VERSION_STRING));
+    ui->labelBuild->setText(QString("Build %1").arg(BUILD_NUMBER_STRING));
+
+    QFile app_license(":/LICENSE.txt");
+    if (app_license.open(QFile::ReadOnly | QFile::Text))
+    {
+        QTextStream app_stream(&app_license);
+        ui->editAppLicense->setText(app_stream.readAll());
+    }
 
     QFile qt_license(":/qt/LICENSE.txt");
     if (qt_license.open(QFile::ReadOnly | QFile::Text))
